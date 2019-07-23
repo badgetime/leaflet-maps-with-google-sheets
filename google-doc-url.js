11fe6de12187be61bd0f23dfd648739e23b49bc4
badgetime <53189713+badgetime@users.noreply.github.com>
2019-07-23 16:07:26 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Zt-CwGatK6_RL4J8bcyZu2SAQ2iBX-UV5FZi09KkfEY/edit#gid=0';
